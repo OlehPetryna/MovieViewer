36a4bdca342e8b451c30f33c7a75052879254ea2
Oleh Petryna <olehpetryna@knu.ua>
2017-12-08 07:37:50 +0200

    drop table if exists

--- initdb.php
+++ initdb.php
@@ -4,13 +4,17 @@ array_shift($argv);
 list($dbHost,$dbName,$dbUser,$dbPass) = $argv;
 
 $pdo = new \PDO("mysql:host=$dbHost;dbname=$dbName;",$dbUser,$dbPass);
+$r = $pdo->exec("DROP TABLE IF EXISTS `Movie`;");
+
+echo "dropped existing table Movie\n";
 
 $sql ="CREATE TABLE Movie(
      id INT( 11 ) AUTO_INCREMENT PRIMARY KEY,
      name VARCHAR( 150 ) NOT NULL, 
      releaseYear INT( 10 ) NOT NULL, 
      format ENUM ('VHS','DVD','Blu-ray') NOT NULL, 
-     actors TEXT NOT NULL) ;";
+     actors TEXT NOT NULL)
+     CHARACTER SET utf8 COLLATE utf8_unicode_ci;";
 $pdo->exec($sql);
 
 echo "created table Movie\n";
