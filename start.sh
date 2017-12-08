4ae9a8a279f190bedc4ff386b2b8efdb820aabe6
Oleh Petryna <mrbomstar@gmail.com>
2017-12-08 09:38:21 +0200

    refactor start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,10 @@
 
 HELP="$(basename "$0") dbHost dbName dbUser dbUserPass"
 HELP0="$(basename "$0") dbHost dbName dbUser"
-if [[ "$#" -ne 4 ]] && !([[ "$#" -eq 3 ]] && [[ -z "$4" ]]); then
-printf "passed wrong arguments, command should be : \n./$HELP"
-printf "\nif user has empty password : \n./$HELP0"
+if [[ "$#" -ne 4 ]] && ! ([[ "$#" -eq 3 ]] && [[ -z "$4" ]]); then
+printf "passed wrong arguments, command should be : \\n./%s" "$HELP"
+printf "\\nif user has empty password : \\n./%s" "$HELP0"
 else
-    php -f initdb.php $1 $2 $3 $4
+    php -f initdb.php "$1" "$2" "$3" "$4"
     php -S localhost:8000 index.php
 fi
-exit
